564dc7150657ecf7fbd330ac352d0c6587cf6489
Anshu Gupta <anshu.gupta351613@gmail.com>
2026-03-15 20:34:53 +0530

    File Positioning

--- src/main.jsx
+++ src/main.jsx
@@ -2,7 +2,7 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
-import Login from './Login.jsx'
+import Login from './pages/Login.jsx'
 import Features from './pages/Features.jsx'
 import { createBrowserRouter, RouterProvider } from 'react-router-dom'
 
