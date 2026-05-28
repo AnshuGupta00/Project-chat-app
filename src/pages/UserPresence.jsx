1f755de63a04d93f1488834f76ad1568eaec78b5
Anshu Gupta <anshu.gupta351613@gmail.com>
2026-05-29 01:31:41 +0530

    Online/Offline Status

---
+++ src/pages/UserPresence.jsx
@@ -0,0 +1,33 @@
+import {useEffect, useState} from 'react';
+import {ref, set, onDisconnect,serverTimestamp} from 'firebase/database';
+import {rtdb} from '../firebase';
+
+export const UserPresence =(userID )=> {
+    useEffect(() => {
+        if (!userID) return;
+        const userStatusRef =ref(rtdb, `status/${userID}`);
+
+        // Set Online when online 
+
+        set(userStatusRef, {
+            state:"online",
+            lastChanged: serverTimestamp()
+        });
+
+        // Auto Set Offline when offline
+
+        onDisconnect(userStatusRef).set({
+            state:"offline",
+            lastChanged: serverTimestamp(),
+        });
+
+        return ()=>{
+            set(userStatusRef,{
+                state:"offline",
+                lastChanged: serverTimestamo(),
+            });
+        };
+
+    },[userID]);
+
+};
